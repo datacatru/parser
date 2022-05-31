c3ae5081de65e2b63944bedec7e5ff36cd07d7d2
Ольга <nensy.08@mail.ru>
2022-05-31 21:11:04 +0300

    скрипт проверки урлов

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 import requests
+import re
+from bs4 import BeautifulSoup
 
+# скачиваем страницу
 url = 'https://medicinadeti.ru/'
-req = requests.get(url)
+req = requests.get(url).text
+soup = BeautifulSoup(req, 'html.parser')
+uniq_page = []
+for link in soup.find_all('a'):
+   pages = link.get('href')
+   if pages:
+       pages = pages.split('#')[0]
+   if pages:
+       if url in pages:
+           pages = pages.replace(url,'')
+       if pages.startswith('http') or pages.startswith('mail') or pages.startswith('tel'):
+           continue
+   uniq_page.append(pages)
+print(uniq_page)
 
-if '50703166' in req.text:
-    print('YM is find')
+
+
+'''
+# перечень страниц
+pages = []
+
+# сохраняем страницу как файл
+with open('med.html', 'w') as file:
+    file.write(req)
+
+# что ищем
+match = re.search(r'ym\([0-9]+,', req) # если с импутом то через f строку
+
+if match:
+    print(match.group(0))
+
+'''
