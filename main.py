7881a49c3159c7503e592d9ec16b6ba9121d361b
Ольга <nensy.08@mail.ru>
2022-06-01 23:59:29 +0300

    добавлена проверка счетчика

--- main.py
+++ main.py
@@ -6,31 +6,38 @@ from bs4 import BeautifulSoup
 url = 'https://medicinadeti.ru/'
 req = requests.get(url).text
 soup = BeautifulSoup(req, 'html.parser')
-uniq_page = []
-for link in soup.find_all('a'):
-   pages = link.get('href')
-   if pages:
-       pages = pages.split('#')[0]
-   if pages:
-       if url in pages:
-           pages = pages.replace(url,'')
-       if pages.startswith('http') or pages.startswith('mail') or pages.startswith('tel'):
-           continue
-   uniq_page.append(pages)
-print(uniq_page)
-
-
-
-'''
-# перечень страниц
-pages = []
-
-# сохраняем страницу как файл
-with open('med.html', 'w') as file:
-    file.write(req)
 
 # что ищем
 match = re.search(r'ym\([0-9]+,', req) # если с импутом то через f строку
+
+# тут список страниц для обхода
+uniq_page = []
+# цикл и условия добавления страниц в список uniq_page
+for link in soup.find_all('a'):
+    if len(uniq_page) < 5:
+        pages = link.get('href')
+        if pages:
+            pages = pages.split('#')[0]
+        if pages:
+            if url in pages:
+                pages = pages.replace(url,'')
+        if pages.startswith('http') or pages.startswith('mail') or pages.startswith('tel'):
+            continue
+        uniq_page.append(pages)
+    else:
+        break
+
+# цикл проверки счетчика на страницах их списка
+for i in uniq_page:
+        checkUrl = requests.get(url + i).text
+        if match.group(0) in checkUrl:
+            print(i,': yes')
+
+
+'''
+    # сохраняем страницу как файл
+    with open('med.html', 'w') as file:
+        file.write(req)
 
 if match:
     print(match.group(0))
