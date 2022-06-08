c2c4d80e8b99ad54181b7d1a89ec0550d082e4ea
Ольга <nensy.08@mail.ru>
2022-06-08 10:54:05 +0300

    добавлен цикл проверки счетчика и сохранения в эксель

--- lib.py
+++ lib.py
@@ -2,26 +2,33 @@ import requests
 import re
 from bs4 import BeautifulSoup
 import pandas
+from lxml import etree as et
 
-
+# получаем html страницы
 def get_page_content(url):
     req = requests.get(url)
     if req.status_code == 200:
         return req.text
 
+# получаем номер GTM
 def extract_gtm(page_content):
-    match = re.search(r'GTM-[0-9a-zA-Z]+', page_content, re.DOTALL)
+    match = re.search(r'GTM-[0-9a-zA-Z]+', page_content)
     if match is not None:
         return match.group(0)
     return "GTM не найден"
 
+# получаем номер яндекс метрики
 def extract_ym(page_content):
-    match = re.search(r'ym\([0-9]+,', req)
+    match = re.search(r'ym\([0-9]+,', page_content)
     if match is not None:
-        return match.group(0)
+        ym = re.search(r'[0-9]+', match.group(0))
+        return ym.group(0)
+
     return "YM не найден"
 
+# получаем ссылки на странице по условиям (на выходе множество)
 def get_links_on_page(page_content, domain):
+    all_pages = set()
     # тут список страниц для обхода
     uniq_page = set()
     soup = BeautifulSoup(page_content, 'html.parser')
@@ -41,7 +48,13 @@ def get_links_on_page(page_content, domain):
             break
     return uniq_page
 
-
-#def get_sitemap(page_content):
-    # сделать парсинг карты сайта https://lxml.de/parsing.html
+'''
+попытка сделать функцию, которая будет получать все ссылки сайта
+def get_all_pages():
+    all_pages = set() # Множество урлов сайта
+    if len(all_pages) < 15:
+        nexturl = get_links_on_page() # следующий урл получает
+        all_pages.add(nexturl)
+    return all_pages
+'''
 
--- main.py
+++ main.py
@@ -1,10 +1,63 @@
-from lib import get_links_on_page,get_page_content,extract_gtm
+from lib import get_links_on_page,get_page_content,extract_gtm, extract_ym
+import xlsxwriter
+import pandas
 
 domain = 'https://medicinadeti.ru/'
 page_content = get_page_content(domain)
 main_gtm = extract_gtm(page_content)
+main_ym = extract_ym(page_content)
 pages = get_links_on_page(page_content, domain)
+all_pages = []
+result = {}
 
+# получаем наличие GTM по ссылкам на странице
 for i in pages:
-    if main_gtm:
-        print(i, main_gtm)
+    if main_gtm or main_ym: # не корректное условие, что если ни того ни другого? Или только одно?
+        result[i] = main_gtm, main_ym
+print(result)
+
+# Делаем DataFrame
+df = pandas.DataFrame.from_dict(result,'index').reset_index()
+df.columns = ['PageUrl', 'GTM', 'YM']
+print(df)
+
+# Сохраняем в excel
+writer = pandas.ExcelWriter('parser.xlsx', engine='xlsxwriter')
+df.to_excel(writer)
+writer.save()
+
+'''
+попытки получить все страницы сайта
+for elem in pages:
+    all_pages.append(elem)
+print(all_pages)
+
+for i in all_pages:
+    content = get_page_content(domain + i) # получаем контент новой страницы
+
+print(content)
+
+            nextpage = get_links_on_page(content, domain) # получем новый список урлов
+            for k in nextpage:
+                all_pages.appenf(k)
+print(all_pages)
+
+   # for nelem in nextpage:
+    #    all_page.add(nelem)
+
+
+
+if len(all_pages)<15:
+    for elem in all_pages:
+        elem = pa
+        for url in pages:
+            all_pages.add(url) # для каждого урл из множества pages добавить элемент в all_pages
+print(all_pages)
+'''
+
+
+
+
+
+
+#all_pages = get_all_pages()
